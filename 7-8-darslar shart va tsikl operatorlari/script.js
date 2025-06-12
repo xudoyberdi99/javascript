19882cb17009ca46b808e3ab9b4c37e5d9d15a02
moliya <developper853@gmail.com>
2025-06-12 16:21:11 +0500

    6-dars tugadi

---
+++ 7-8-darslar shart va tsikl operatorlari/script.js
@@ -0,0 +1,18 @@
+let numberOfSeries = +prompt(`nechta serial ko'rdingiz`, '')
+// console.log(numberOfSeries)
+
+while (numberOfSeries === 0) {
+	numberOfSeries = +prompt(`nechta serial ko'rdingiz`, '')
+	if (numberOfSeries != 0) {
+		console.log(numberOfSeries)
+		break
+	}
+}
+
+if (numberOfSeries < 5) {
+	alert('kam serial koryapsz')
+} else if (numberOfSeries > 5 && numberOfSeries < 10) {
+	alert('siz classik tomoshabin ekansz')
+} else {
+	alert('sizdan zuri yuq')
+}
