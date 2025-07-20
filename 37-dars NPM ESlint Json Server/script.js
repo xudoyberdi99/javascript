92a8e12ddf16e98042f18a0e0cb7d5ecca9ef08c
Xudoyberdi <xudoyberdi3003@gmail.com>
2025-07-20 22:19:45 +0500

    java Script The end

--- 37-dars NPM ESlint Json Server/script.js
+++ 37-dars NPM ESlint Json Server/script.js
@@ -1,2 +1,52 @@
 // ESlint bizga xatolarni ko'rsatadi va to'g'rilashga yordam beradi
 'use strict' //qatiy eski kodlarni ishlatmaslik uchun
+localStorage.setItem('sammi-theme', 'light')
+
+const theme = localStorage.getItem('sammi-theme')
+
+if (theme === 'light') {
+	document.body.style.backgroundColor = '#eee'
+} else if (theme === 'dark') {
+	document.body.style.backgroundColor = '#1f1f1f'
+}
+
+const form = document.querySelector('form'),
+	postParent = document.querySelector('.posts')
+
+form.addEventListener('submit', event => {
+	event.preventDefault()
+
+	const formData = new FormData(form)
+
+	const object = {}
+
+	formData.forEach((value, key) => {
+		object[key] = value
+	})
+
+	const posts = []
+
+	posts.push(object)
+
+	const db = JSON.parse(localStorage.getItem('posts'))
+
+	if (db) {
+		localStorage.setItem('posts', JSON.stringify([...db, object]))
+	} else {
+		localStorage.setItem('posts', JSON.stringify([object]))
+	}
+})
+
+getPosts()
+
+function getPosts() {
+	const posts = JSON.parse(localStorage.getItem('posts'))
+	posts.forEach((item, index) => {
+		const postElement = document.createElement('div')
+		postElement.classList.add('post')
+		postElement.innerHTML = `	<h4><b>#${index + 1}.</b>  ${item.title}</h4>
+					<p>${item.body}</p>
+		`
+		postParent.append(postElement)
+	})
+}
